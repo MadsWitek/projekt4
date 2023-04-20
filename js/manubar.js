65a47adebeb35a23b5c1a28fc38fba5f8e01520e
nittayamatet <119924064+nittayamatet@users.noreply.github.com>
2023-04-21 01:25:07 +0200

    pil-up

--- js/manubar.js
+++ js/manubar.js
@@ -33,3 +33,23 @@ function normalImg(x) {
 }
 
 
+let mybutton = document.getElementById("scrolle");
+           
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+           
+ 
