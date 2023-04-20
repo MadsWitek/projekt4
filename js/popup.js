23a2bb65d519e40e8921d0bbd3a62dd125e6159e
AndersBuch <119923346+AndersBuch@users.noreply.github.com>
2023-04-20 12:04:44 +0200

    added popup css
    dd

--- js/popup.js
+++ js/popup.js
@@ -3,7 +3,7 @@ console.log('DOM Loaded');
 
 function setup() {
     const options = {
-        rootMargin: '-60px 0px -60px 0px'
+        rootMargin: '-60px -0px -0px -60px'
     }
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
@@ -16,21 +16,7 @@ function setup() {
         })
     }, options)
 
-    const h2 = document.querySelectorAll(".overskrift");
-    h2.forEach(overskrift => observer.observe(overskrift));
+    const indleag = document.querySelectorAll(".container");
+    indleag.forEach(container => observer.observe(container));
 
-    const h = document.querySelectorAll(".overskrift1");
-    h.forEach(overskrift1 => observer.observe(overskrift1));
-    
-    const h3 = document.querySelectorAll(".overskrift2");
-    h3.forEach(overskrift2 => observer.observe(overskrift2));
-
-    const p = document.querySelectorAll(".tekst");
-    p.forEach(tekst => observer.observe(tekst));
-    
-    const box = document.querySelectorAll(".box1");
-    box.forEach(box1 => observer.observe(box1));
-
-    const boxx = document.querySelectorAll(".box2");
-    boxx.forEach(box2 => observer.observe(box2));
 }
