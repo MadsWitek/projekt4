9786ee68bd397ffc8d603d662a197e2926680354
nittayamatet <119924064+nittayamatet@users.noreply.github.com>
2023-04-21 00:57:32 +0200

    resou

--- js/manubar.js
+++ js/manubar.js
@@ -11,3 +11,25 @@ function changeElement() {
 
 
 
+// Add active class to the current button (highlight it)
+var header = document.getElementById("nav-bar");
+var btns = header.getElementsByClassName("bb");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+function bigImg(x) {
+  x.style.height = "80px";
+  x.style.width = "80px";
+}
+
+function normalImg(x) {
+  x.style.height = "32px";
+  x.style.width = "32px";
+}
+
+
