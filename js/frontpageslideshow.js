ee8e20934621918b903c5b8b327339732b0e14d7
Bakinator <Gustavbak@live.dk>
2023-04-21 06:04:45 +0200

    aaa

--- js/frontpageslideshow.js
+++ js/frontpageslideshow.js
@@ -10,7 +10,7 @@ let ba, bi=0, intrvl;
 let bca = [
     '<div class="slide-indhold"><h2><b>WORKSHOP I WORDPRES</b></h2><br><p>1 semester · workshop · wordpres<br><br>Jeg vil rigtig gerne have mere vejledning i wordpres, da jeg føler vi fik en meget kort introduktion til det. Er der stemning for at lave en workshop på tværs af semestrene?</p></div>',
     '<div class="slide-indhold"><h2><b>FLERE UNDERVISNINGSTIMER I HTML & CSS</b></h2><br><p>1 semester · Almen undervisning · HTML & CSS<br><br> Jeg er lige startet på 1 semester og synes at HTML & CSS er svært. Det gik op for mig at vi kun har 5 lektioner til at lære det hvilket ikke er særlig langt tid. Kunne det være relevant med mere undervisning i HTML & CSS? Hvad tænker i?</p></div>',
-    '<div class="slide-indhold"><h2><b>MERE VEJLEDNING & FEEDBACK I LØBET AF PROJEKTER</b></h2><br><p>2 semester · Vejledning · Projektarbejde <br><br>Jeg vil rigtig gerne have mere vejledning og feedback i løbet af projekterne. Lige nu føler jeg at det hele ligger meget samlet og ikke særlig spredt ud? Er der andre der har det på sammme måde? <br></p></div>',
+    '<div class="slide-indhold"><h2><b>MERE VEJLEDNING & FEEDBACK I LØBET AF PROJEKTER</b></h2><br><p>2 semester · Vejledning · Teori <br><br>Jeg vil rigtig gerne have mere vejledning og feedback i løbet af projekterne. Lige nu føler jeg at det hele ligger meget samlet og ikke særlig spredt ud? Er der andre der har det på sammme måde? <br></p></div>',
     '<div class="slide-indhold"><h2><b>MERE UNDERVISNING I SCRUM</b></h2><br><p>1 semester · Almen undervisning · Scrum <br><br>Jeg synes det kunne være fedt hvis vi kunne få lidt mere undervisning i scrum og projekstyring, da jeg synes det er lidt forvirrrende. Hvad tænker i?</p></div>',  
 ];
 
